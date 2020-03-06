9e54618c8a57112c66bac9f1a808c0d6f57aa094
Justin Menestrina <jmenestr@gmail.com>
2020-03-06 09:20:27 -0700

    fix

---
+++ src/ComponentTwo.tsx
@@ -0,0 +1,12 @@
+import {Paragraph} from '@guildeducationinc/recess/components/Typography';
+import { Select } from '@guildeducationinc/recess/components/Select';
+
+export const One = () => {
+  return (
+    <div>
+      <Paragraph>Hello</Paragraph>
+      <Paragraph>Hello</Paragraph>
+      <Select />
+    </div>
+  )
+}
