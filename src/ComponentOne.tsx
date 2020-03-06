ae73e68f9c7cff2c2fbaf5b400b3e40ee352b3d7
Justin Menestrina <jmenestr@gmail.com>
2020-03-06 09:16:38 -0700

    test

---
+++ src/ComponentOne.tsx
@@ -0,0 +1,13 @@
+import {Paragraph} from '@guildeducationinc/recess/components/Typography';
+import { TextArea } from '@guildeducationinc/recess/components/TextArea';
+
+export const One = () => {
+  return (
+    <div>
+      <Paragraph>Hello</Paragraph>
+      <TextArea>
+        Wooo
+      </TextArea>
+    </div>
+  )
+}
